3cc0e680ed094ba97cde19c1a9a9d2c65449ce16
guxal <jonathanacp93@gmail.com>
2022-06-13 20:58:05 -0500

    refactor(frontend): move html to react

---
+++ frontend/assets/img/random/index.js
@@ -0,0 +1,10 @@
+import circus from './circus.png'
+import cake from './cake.png'
+import game from './game.png'
+import safe from './safe.png'
+import submarine from './submarine.png'
+import cabin from './cabin.png'
+
+
+
+export { circus, cake, game, safe, submarine, cabin }
