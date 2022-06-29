2b0b4b6c34704de428bfadf14094f71b9b5fbc73
unknown <cruzyumari2@gmail.com>
2022-06-29 00:01:43 -0500

    feat: before the metaverse and marketplace components

--- frontend/pages/Maps/IndexMaps.jsx
+++ frontend/pages/Maps/IndexMaps.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from 'mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import { tokenMapBox } from '../../utils/mapboxUtils'
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoicmVhbGl0eS1uZWFyLWRldnMiLCJhIjoiY2wzejY1Zm1tMXV1MTNicDRtZzViazRsNSJ9.Br1q0_qFPtNqIr59i-MqIA'
-
+mapboxgl.accessToken = tokenMapBox
 export default function IndexMaps() {
-  //   const mapContainer = useRef(null)
-  //   const map = useRef(null)
-  //   const [lng, setLng] = useState(-70.9)
-  //   const [lat, setLat] = useState(42.35)
-  //   const [zoom, setZoom] = useState(9)
-  return <div>IndexMaps</div>
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom,
+    })
+  })
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container h-50vh"></div>
+    </div>
+  )
 }
