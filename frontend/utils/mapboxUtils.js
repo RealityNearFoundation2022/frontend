cd6e02bccb6cfbce1f958902f7f18f5536eecdef
yud-cumba <yudith.cumba@gmail.com>
2022-07-05 15:57:11 -0500

    feat: add maps

--- frontend/utils/mapboxUtils.js
+++ frontend/utils/mapboxUtils.js
@@ -1,3 +1,28 @@
 /* eslint-disable prettier/prettier */
 export const tokenMapBox =
   'pk.eyJ1IjoicmVhbGl0eS1uZWFyLWRldnMiLCJhIjoiY2wzejY1Zm1tMXV1MTNicDRtZzViazRsNSJ9.Br1q0_qFPtNqIr59i-MqIA'
+const PARCEL_SIZE = 1;
+const VERTICAL = {
+  positive: 100,
+  negative: 70
+}
+
+
+export const coordinates = () => {
+  // VERTICAL POSITIVE
+  const coord =   []
+  const parcelV = {
+    positive:VERTICAL.positive/PARCEL_SIZE,
+    negative:VERTICAL.negative/PARCEL_SIZE,
+  };
+  for (let i = 0; i < parcelV.positive; i += 1) {
+    coord.push([
+      [0, 0+(i*PARCEL_SIZE)],
+      [0+PARCEL_SIZE, 0+(i*PARCEL_SIZE)],
+      [0+PARCEL_SIZE, 0+((i+1)*PARCEL_SIZE)],
+      [0,0+((i+1)*PARCEL_SIZE)],
+    ])
+  }
+  console.log(coord);
+  return coord;
+}
