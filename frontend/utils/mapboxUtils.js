f8df46b968de9703a1e16bcce947966634be52e3
yud-cumba <yudith.cumba@gmail.com>
2022-07-14 17:52:22 -0500

    feat: add

--- frontend/utils/mapboxUtils.js
+++ frontend/utils/mapboxUtils.js
@@ -1,28 +1,40 @@
 /* eslint-disable prettier/prettier */
 export const tokenMapBox =
   'pk.eyJ1IjoicmVhbGl0eS1uZWFyLWRldnMiLCJhIjoiY2wzejY1Zm1tMXV1MTNicDRtZzViazRsNSJ9.Br1q0_qFPtNqIr59i-MqIA'
-const PARCEL_SIZE = 1;
+const PARCEL_SIZE = 0.001;
 const VERTICAL = {
-  positive: 100,
-  negative: 70
+  positive: 1,
+  negative: 0.7
 }
 
 
-export const coordinates = () => {
+export const coordinatesGeojson = (lat,lng) => {
   // VERTICAL POSITIVE
   const coord =   []
-  const parcelV = {
-    positive:VERTICAL.positive/PARCEL_SIZE,
-    negative:VERTICAL.negative/PARCEL_SIZE,
-  };
-  for (let i = 0; i < parcelV.positive; i += 1) {
-    coord.push([
-      [0, 0+(i*PARCEL_SIZE)],
-      [0+PARCEL_SIZE, 0+(i*PARCEL_SIZE)],
-      [0+PARCEL_SIZE, 0+((i+1)*PARCEL_SIZE)],
-      [0,0+((i+1)*PARCEL_SIZE)],
-    ])
+  // const parcelV = {
+  //   positive:VERTICAL.positive/PARCEL_SIZE,
+  //   negative:VERTICAL.negative/PARCEL_SIZE,
+  // };
+  // for(j = 0; j < 20; j +=1) {
+  for (let i = 0; i < 20; i += 1) {
+    const geojson =    {
+      type: 'Feature',
+      id: `geojson${i}`,
+      properties: {
+        value: 0.2523820479083345,
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [lat, lng+(i*PARCEL_SIZE)],
+          [lat+PARCEL_SIZE, lng+(i*PARCEL_SIZE)],
+          [lat+PARCEL_SIZE, lng+((i+1)*PARCEL_SIZE)],
+          [lat,lng+((i+1)*PARCEL_SIZE)],
+        ]
+      },
+    }
+    coord.push(geojson)
+  // }
   }
-  console.log(coord);
   return coord;
 }
