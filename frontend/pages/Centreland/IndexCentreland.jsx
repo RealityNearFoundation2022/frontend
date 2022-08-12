c5bf940d4ab5242c92121d2b94798f7ba4db3775
yud.cumba <yuditd.cumba@gmail.com>
2022-08-11 19:33:55 -0500

    feat: add map tilemap

--- frontend/pages/Centreland/IndexCentreland.jsx
+++ frontend/pages/Centreland/IndexCentreland.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react'
 import TileMap from '../../utils/tilemap'
 export default function IndexCentreland() {
-  let canvas
-  let ctx
-  const tileSize = 5
-  const tileMap = new TileMap(tileSize)
-  useEffect(() => {
-    canvas = document.getElementById('centreland')
-    ctx = canvas.getContext('2d')
-    gameLoop()
-    gameLoop()
-    gameLoop()
+  const tileSize = 15
 
-  })
-  const gameLoop = () => {
+  // let canvas
+  // let ctx
+  // const tileSize = 5
+  // const tileMap = new TileMap(tileSize)
+  useEffect(() => {
+    const canvas = document.getElementById('centreland')
+    const ctx = canvas.getContext('2d')
+
+    const tileMap = new TileMap(tileSize)
+    console.log(canvas)
     tileMap.draw(canvas, ctx)
-  }
+
+    // gameLoop()
+
+  }, [])
+  // const gameLoop = () => {
+  //   tileMap.draw(canvas, ctx)
+  // }
  
   return (
     <div className="top">
