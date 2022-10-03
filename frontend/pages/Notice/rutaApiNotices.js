13ef450a8791241880fd16721aea422b025b642b
Cruz <cruzyumari2@gmail.com>
2022-10-03 12:21:20 -0500

    merge 03Oct

--- frontend/pages/Notice/rutaApiNotices.js
+++ frontend/pages/Notice/rutaApiNotices.js
@@ -1,3 +1,4 @@
 /* eslint-disable prettier/prettier */
-export const api = 'http://45.77.115.23:8081'
+// export const api = 'http://45.77.115.23:8081'
+export const api = 'https://api.realitynear.org'
 
