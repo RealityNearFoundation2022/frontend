e2c1f6cccf060b9a04b769245e7ff3290f08cbed
yud.cumba <yuditd.cumba@gmail.com>
2022-11-08 12:47:58 -0500

    feat: add new link

---
+++ frontend/components/FollowInfo.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import twitterIcon from '../assets/img/social-network/twitter.png'
+import githubIcon from '../assets/img/social-network/githubIcon.png'
+import discordIcon from '../assets/img/social-network/discordIcon.png'
+import fbIcon from '../assets/img/social-network/facebook.png'
+
+export default function FollowInfo({ isBackground }) {
+  const socialNetworks = [
+    {
+      icon: discordIcon,
+      link: 'https://discord.gg/jWBXDjU6',
+    },
+    {
+      icon: twitterIcon,
+      link: 'https://twitter.com/RealityNearOrg',
+    },
+    {
+      icon: fbIcon,
+      link: 'https://www.facebook.com/FundacionRealityNear',
+    },
+
+    {
+      icon: githubIcon,
+      link: 'https://github.com/RealityNearFoundation2022/',
+    },
+  ]
+  return (
+    <div
+      className={`p-5 bg-img-size-cover w-100 ${
+        isBackground && 'bg-img-realExperience text-white'
+      }`}
+    >
+      <h1 className="d-flex justify-content-center m-0 h-50 align-items-center w-100 fs-7">
+        ¡SÍGUENOS!
+      </h1>
+      <div className="d-flex justify-content-center m-0 h-50 align-items-center w-100 py-5">
+        {socialNetworks.map(({ link, icon }) => (
+          <a href={link} target="_blank">
+            <img src={icon} alt="" className="px-2 icon" />
+          </a>
+        ))}
+      </div>
+    </div>
+  )
+}
--- frontend/pages/Landing/RealityExperience.jsx
+++ frontend/pages/Landing/RealityExperience.jsx
@@ -1,10 +1,7 @@
 import React, { useContext } from 'react'
 import logoRealityNear from '../../assets/img/random/LOGO REALITY NEAR.png'
 import ThemeContext from '../../utils/useContextTheme'
-import discordIcon from '../../assets/img/social-network/discordIcon.png'
-import igIcon from '../../assets/img/social-network/instagramIcon.png'
-import githubIcon from '../../assets/img/social-network/githubIcon.png'
-import frIcon from '../../assets/img/social-network/free-redditIcon.png'
+import FollowInfo from '../../components/FollowInfo'
 
 export default function RealityExperience() {
   const { theme } = useContext(ThemeContext)
@@ -34,17 +31,7 @@ export default function RealityExperience() {
           </button>
         </div>
       </div>
-      <div className="h-30vh p-5 mt-5">
-        <h1 className="d-flex justify-content-center m-0 h-50 align-items-center w-100 fs-7 link-primary">
-          ¡SÍGUENOS!
-        </h1>
-        <div className="d-flex justify-content-center m-0 h-50 align-items-center w-100 py-5">
-          <img src={discordIcon} alt="" className="px-2" />
-          <img src={igIcon} alt="" className="px-2" />
-          <img src={githubIcon} alt="" className="px-2" />
-          <img src={frIcon} alt="" className="px-2" />
-        </div>
-      </div>
+      <FollowInfo />
     </section>
   )
 }
