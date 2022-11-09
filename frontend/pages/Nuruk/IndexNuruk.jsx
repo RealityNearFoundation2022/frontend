3a0ce218dfff81b12251ebc24ba6123fdf44fa32
yud.cumba <yuditd.cumba@gmail.com>
2022-11-09 16:57:46 -0500

    feature: add components

--- frontend/pages/Nuruk/IndexNuruk.jsx
+++ frontend/pages/Nuruk/IndexNuruk.jsx
@@ -4,14 +4,10 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import Modal from '@mui/material/Modal'
-import Box from '@mui/material/Box'
 import { useNavigate } from 'react-router-dom'
-
+import ModalBuy from '../../components/ModalBuy'
 import TileMap from '../../utils/tilemap'
 import MockUp from '../../assets/img/MapaMockUp.jpg'
-import WhiteIcon from '../../assets/img/logo-white.svg'
-import { useNavigate } from 'react-router-dom'
 
 export default function IndexCentreland() {
   const tileSize = 15
@@ -32,19 +28,6 @@ export default function IndexCentreland() {
   const [posX, setPosX] = useState(0)
   const [posY, setPosY] = useState(0)
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  }
   const getPosition = (e) => {
     const cursorX = e.pageX
     const cursorY = e.pageY
@@ -60,9 +43,7 @@ export default function IndexCentreland() {
     setPosY(posy)
     setOpen(true)
   }
-  const handleClose = () => {
-    setOpen(false)
-  }
+
   useEffect(() => {
     getCentreland(row, column)
   }, [column, row])
@@ -122,7 +103,9 @@ export default function IndexCentreland() {
   function goToPlotNurk(position) {
     navigate(`/nuruk/${position}`)
   }
-
+  const handleClose = () => {
+    setOpen(false)
+  }
   const getCentreland = (_row, _column) => {
     const canvas = document.getElementById(`centreland${_row}-${_column}`)
     const ctx = canvas.getContext('2d')
@@ -181,66 +164,12 @@ export default function IndexCentreland() {
         </button>
         <div className={`boxImg boxImg-${row}${column}`}></div>
       </div>
-      <Modal
+      <ModalBuy
         open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={{
-            ...style,
-            width: 700,
-            borderRadius: '20px',
-            border: 0,
-            padding: 2,
-          }}
-        >
-          <div className="row">
-            <div className="col col-md-3">
-              <img
-                src="https://via.placeholder.com/500"
-                className="rounded"
-                style={{ maxWidth: '100%' }}
-                alt=""
-              />
-            </div>
-            <div className="col col-md-5 p-2">
-              <h2 className="h4" id="child-modal-title">
-                Realand #{posX}
-                {posY}
-              </h2>
-              <p id="child-modal-description" className="h5 text-grey">
-                <span className="pr-2">
-                  <img
-                    src={WhiteIcon}
-                    className="bg-primary rounded-pill"
-                    style={{ width: 30 }}
-                    alt=""
-                  />
-                </span>
-                10000.000000
-              </p>
-              <p className="h5 font-weight-bold text-grey">Disponible</p>
-            </div>
-            <div
-              style={{ minHeight: 150 }}
-              className="col col-md-4 d-flex align-items-center justify-content-center"
-            >
-              <div>
-                <button
-                  type="button"
-                  onClick={function () { return goToPlotNurk(`${posX}${posY}`) }}
-                  className="rounded btn-xl btn-primary"
-                  style={{ shadow: 'none', border: 'none' }}
-                >
-                  Comprar
-                </button>
-              </div>
-            </div>
-          </div>
-        </Box>
-      </Modal>
+        handleClose={handleClose}
+        go={() => goToPlotNurk(`${posX}${posY}`)}
+        id={`${posX}${posY}`}
+      />
     </div>
   )
 }
