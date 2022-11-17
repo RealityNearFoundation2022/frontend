ed82a50de3c9869ca4f777d45a587fcb3d9ffaa0
yud.cumba <yuditd.cumba@gmail.com>
2022-11-17 15:12:48 -0500

    feat: add img to nuruk

--- frontend/pages/ErrorPage/NoData.jsx
+++ frontend/pages/ErrorPage/NoData.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import noData from '../../assets/img/error-page/no-data.png'
 import folder from '../../assets/img/error-page/folder.svg'
-import { Link } from 'react-router-dom'
 
 export default function NoData() {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="row">
         <div className="col-12 col-md-7 d-none d-md-block">
-          <img src={noData} className='w-100' />
+          <img src={noData} className="w-100" />
         </div>
         <div className="col-12 col-md-5 d-flex align-items-center py-5">
-          <div className='text-center text-grey'>
-            <img src={folder} style={{width: 140}} alt="" />
-            <h1 className='h3 mt-3'>¡Lo sentimos! No encontramos datos</h1>
-            <div className='mt-3'>
+          <div className="text-center text-grey">
+            <img src={folder} style={{ width: 140 }} alt="" />
+            <h1 className="h3 mt-3">¡Lo sentimos! No encontramos datos</h1>
+            <div className="mt-3">
               <p>Es probable que no contemos con los datos que buscas.</p>
               <p>¡Intenta ajustar los filtros y buscar otra vez!.</p>
             </div>
-            <Link to='/'>
-              <button className="_btn btn-primary btn-xl rounded mt-3">Home</button>
+            <Link to="/">
+              <button className="_btn btn-primary btn-xl rounded mt-3">
+                Home
+              </button>
             </Link>
           </div>
         </div>
