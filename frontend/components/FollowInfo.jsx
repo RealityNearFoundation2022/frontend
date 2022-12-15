81dfedc95da0154cc9c815fa96fd703d788be674
Christian Paredes Rivera <christian.fernando.paredes@gmail.com>
2022-12-15 16:16:10 -0500

    feat: follow us theme color

--- frontend/components/FollowInfo.jsx
+++ frontend/components/FollowInfo.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import twitterIcon from '../assets/img/social-network/twitter.png'
+import githubIconWhite from '../assets/img/social-network/github-icon-white.png'
 import githubIcon from '../assets/img/social-network/githubIcon.png'
 import discordIcon from '../assets/img/social-network/discordIcon.png'
 import fbIcon from '../assets/img/social-network/facebook.png'
 import PropTypes from 'prop-types'
+<<<<<<< Updated upstream
 import { useTranslation } from 'react-i18next'
 
 export default function FollowInfo({ isBackground }) {
   const { t } = useTranslation()
 
+=======
+import ThemeContext from '../utils/useContextTheme'
+
+export default function FollowInfo({ isBackground }) {
+  const { theme } = useContext(ThemeContext)
+>>>>>>> Stashed changes
   const socialNetworks = [
     {
       icon: discordIcon,
@@ -24,8 +32,9 @@ export default function FollowInfo({ isBackground }) {
     },
 
     {
-      icon: githubIcon,
+      icon: theme.bg === 'bg-light' ? githubIcon : githubIconWhite,
       link: 'https://github.com/RealityNearFoundation2022/',
+      linkIconWhite: githubIconWhite
     },
   ]
   return (
@@ -34,8 +43,13 @@ export default function FollowInfo({ isBackground }) {
         isBackground && 'bg-img-realExperience text-white'
       }`}
     >
+<<<<<<< Updated upstream
       <h1 className="d-flex justify-content-center m-0 h-50 align-items-center w-100 fs-7">
         { t('SIGUENOS') }
+=======
+      <h1 className={`d-flex justify-content-center m-0 h-50 align-items-center w-100 fs-7 ${theme.txt}`}>
+        ¡SÍGUENOS!
+>>>>>>> Stashed changes
       </h1>
       <div className="d-flex justify-content-center m-0 h-50 align-items-center w-100 py-5">
         {socialNetworks.map(({ link, icon }) => (
